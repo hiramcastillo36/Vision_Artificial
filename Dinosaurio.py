b64ce6f6060a9a1ef03e80eb3afaa54a18921aad
Hiram Castillo <hiramcastillo36@gmail.com>
2020-04-09 17:31:57 -0600

    Update Dinosaurio.py

--- Dinosaurio.py
+++ Dinosaurio.py
@@ -12,36 +12,26 @@ Smin=123
 Smax=255
 Vmin=44
 Vmax=255
+minimo=np.array([Hmin,Smin,Vmin])
+maximo=np.array([Hmax,Smax,Vmax])
 
-contador=0
-last_time = time.time()
 while(True):
-    minimo=np.array([Hmin,Smin,Vmin])
-    maximo=np.array([Hmax,Smax,Vmax])
     # 800x600 windowed mode
-    printscreen =  np.array(ImageGrab.grab(bbox=(0,40,410,585)))
-    gray= cv2.cvtColor(printscreen, cv2.COLOR_BGR2GRAY)
-    #cv2.imshow('gray',gray)
-    blur= cv2.GaussianBlur(printscreen,(7,7),0)
-    #cv2.imshow('blur',blur)
+    printscreen =  np.array(ImageGrab.grab(bbox=(1,40,380,650)))
     hsv= cv2.cvtColor(printscreen, cv2.COLOR_BGR2HSV)
-    #cv2.imshow('hsv',hsv)
     union= cv2.inRange(hsv, minimo, maximo)
     f1= cv2.erode(union, cv2.getStructuringElement(cv2.MORPH_RECT,(3,3)), iterations=1)
     f2= cv2.erode(f1, cv2.getStructuringElement(cv2.MORPH_RECT,(5,5)), iterations=1)
     object= cv2.moments(f2)
-    last_time = time.time()
-    #cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
-
-    if object['m00'] > 10:
+    if object['m00'] > 5:
         cx= int(object['m10']/object['m00'])
         cy= int(object['m01']/object['m00'])
-        cv2.circle(printscreen, (cx,cy), 15, (0,255,0), 2)
-        if cx > 150 and cx < 380 and cy> 280:
+        cv2.circle(printscreen, (cx,cy), 20, (0,255,0), 1)
+        if cx > 230 and cx < 360 and cy>280:
             shell.SendKeys("{UP}")
-            contador=contador+1
 
-    cv2.imshow('xd',printscreen)
+
+    #cv2.imshow('xd',printscreen)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
